b9ef84f1cc2e2e5b6b8e3583f93f1ba091b116a5
Chirkov Petro <tchirkov.petter@gmail.com>
2025-02-16 23:57:32 +0200

    new plugins

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,15 +1,15 @@
--- return {
--- 	{
--- 		"akinsho/bufferline.nvim",
--- 		-- version = '*',
--- 		dependencies = "nvim-tree/nvim-web-devicons",
--- 		config = function()
--- 			vim.opt.termguicolors = true
--- 			require("bufferline").setup({
--- 				vim.keymap.set({ "n" }, "<S-l>", "<cmd>BufferLineCycleNext<CR>"),
--- 				vim.keymap.set({ "n" }, "<S-h>", "<cmd>BufferLineCyclePrev<CR>"),
--- 				vim.keymap.set({ "n" }, "<Leader>x", "<cmd>bd<CR>"),
--- 			})
--- 		end,
--- 	},
--- }
+return {
+	{
+		"akinsho/bufferline.nvim",
+		-- version = '*',
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			vim.opt.termguicolors = true
+			require("bufferline").setup({
+				vim.keymap.set({ "n" }, "<S-l>", "<cmd>BufferLineCycleNext<CR>"),
+				vim.keymap.set({ "n" }, "<S-h>", "<cmd>BufferLineCyclePrev<CR>"),
+				vim.keymap.set({ "n" }, "<Leader>x", "<cmd>bd<CR>"),
+			})
+		end,
+	},
+}
